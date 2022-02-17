a2bfec574e4527af50efcb7705436b4e3b7bfeaa
Fidel Lim <lim.fidel@gmail.com>
2022-02-18 01:42:40 +0400

    test files for mandatory

--- tests/main.c
+++ tests/main.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: flim <flim@student.42abudhabi.ae>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/12 09:14:39 by aperez-b          #+#    #+#             */
-/*   Updated: 2022/02/17 16:29:09 by flim             ###   ########.fr       */
+/*   Created: 2021/07/12 09:14:39 by flim          #+#    #+#             */
+/*   Updated: 2022/02/17 23:09:52 by flim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,14 @@
 
 int	main(void)
 {
-	int		fd;
+	int		fd, fd2;
 	int		i;
 	char	*line[MAX_FD];
 	char	*arr_of_filenames[] = {
 		// "gnlTester/files/big_line_no_nl",
 		"tests/test2.txt",
 		"tests/sample.txt",
+		"tests/test.txt",
 	};
 	int		file_counter;
 
@@ -49,7 +50,57 @@ int	main(void)
 			i++;
 		}
 		printf("\n----------------------------\n");
-		//close(fd);
+		close(fd);
 		file_counter++;
 	}
+	// file_counter = 0;
+	// i = 0;
+	// fd = open(arr_of_filenames[file_counter], O_RDONLY);
+	// printf("----------------------------\n");
+	// printf("Filename: %s\n", arr_of_filenames[file_counter]);
+	// line[fd] = get_next_line(fd);
+	// printf("Line %d for fd %d: %s", i++, fd, line[fd]);
+	// free(line[fd]);
+	// line[fd] = get_next_line(fd);
+	// printf("Line %d for fd %d: %s", i++, fd, line[fd]);
+	// free(line[fd]);
+	// 	line[fd] = get_next_line(fd);
+	// printf("Line %d for fd %d: %s", i++, fd, line[fd]);
+	// free(line[fd]);
+	// close(fd);
+	// printf("\n");
+	// // i= 0;
+	// // file_counter++;
+	// // fd = open(arr_of_filenames[file_counter], O_RDONLY);
+	// // printf("Filename: %s\n", arr_of_filenames[file_counter]);
+	// // line[fd] = get_next_line(fd);
+	// // printf("Line %d for fd %d: %s", i, fd, line[fd]);
+	// // free(line[fd]);
+
+	// i = 0;
+	// file_counter = 0;
+	// fd = open(arr_of_filenames[file_counter], O_RDONLY);
+	// printf("Filename: %s\n", arr_of_filenames[file_counter]);
+	// line[fd] = get_next_line(fd);
+	// printf("Line %d for fd %d: %s", i, fd, line[fd]);
+	// free(line[fd]);
+
+	// file_counter++;
+	// fd2 = open(arr_of_filenames[file_counter], O_RDONLY);
+	// printf("Filename: %s\n", arr_of_filenames[file_counter]);
+	// line[fd2] = get_next_line(fd2);
+	// printf("Line %d for fd %d: %s", i, fd2, line[fd2]);
+	// free(line[fd2]);
+
+	// i = 0;
+	// file_counter = 0;
+	// printf("Filename: %s\n", arr_of_filenames[file_counter]);
+	// line[fd] = get_next_line(fd);
+	// printf("Line %d for fd %d: %s", i, fd, line[fd]);
+	// free(line[fd]);
+
+	// printf("Filename: %s\n", arr_of_filenames[file_counter]);
+	// line[fd2] = get_next_line(fd2);
+	// printf("Line %d for fd %d: %s", i, fd2, line[fd2]);
+	// free(line[fd2]);
 }
